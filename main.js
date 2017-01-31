6209fb88157ff2e4a7e80aafe0960e0214b49b8c
Ben Greaves <bsgreaves@gmail.com>
2017-01-31 02:40:42 -0600

    App is working! Colors works. Values validate. etc. Need to make enter key event listener and then work on layout.

--- main.js
+++ main.js
@@ -9,10 +9,69 @@ var clearButton = document.getElementById("clearButton");
 
 //FUNCTION DECLARATIONS
 
-//**Function to turn user input into a number
 function parseInput(input) {
-	return parseFloat(input); 
+	return input = parseFloat(input);
 }
+
+function toCelsius(fahrenheit) {
+	var celsius = (fahrenheit - 32) * (5/9);
+	return celsius = Math.round(celsius);
+}
+
+function toFahrenheit (celsius) {
+	var fahrenheit = (celsius) * (9/5) + 32;
+	return fahrenheit = Math.round(fahrenheit);
+}
+
+function fahDetermineColor (temp) {
+	if (temp > 90) {
+		return "red";
+	} else if (temp < 32) {
+		return "blue";
+	} else {
+		return "green";
+	}
+}
+
+function celDetermineColor (temp) {
+	if (temp > 32) {
+		return "red";
+	} else if (temp < 0) {
+		return "blue";
+	} else {
+		return "green";
+	}
+}
+
+function outputToDiv (div, color, temp, scale) {
+	return div.innerHTML = "<span class='" + color + "'>" + temp + " degrees " + scale + "</span>";
+}
+
+function convertTemp() {
+	var userTemp = parseInput(document.getElementById("userTemp").value);
+	if (isNaN(userTemp)) {
+		return outputDiv.innerHTML = "Please type a number.";
+	}
+	if (fahToCelRadio.checked) {
+		userTemp = toCelsius(userTemp);
+		outputColor = celDetermineColor(userTemp);
+		return outputToDiv(outputDiv, outputColor, userTemp, "Celsius");
+	} else if (celToFahRadio.checked) {
+		userTemp = toFahrenheit(userTemp);
+		outputColor = fahDetermineColor(userTemp);
+		return outputToDiv(outputDiv, outputColor, userTemp, "Fahrenheit");
+	} else {
+		return outputDiv.innerHTML = "Please select whether to convert to celsius or fahrenheit.";
+	}
+}
+
+//EVENT LISTENERS
+
+clearButton.addEventListener("click", function() {
+	document.getElementById("tempconverter").reset();
+	outputDiv.innerHTML = "";
+})
+convertButton.addEventListener("click", convertTemp);
 
 //** Assign a function to be executed when the user presses enter 
 // document.addEventListener("keypress", function (e) {
@@ -20,77 +79,6 @@ function parseInput(input) {
 // 		determineConverter();
 // 	}
 // }
-
-//**Function: F to C
-function toCelsius(fahrenheit) {
-	var celsius = (fahrenheit - 32) * (5/9);
-	return celsius;
-}
-
-
-//**Function: C to F
-function toFahrenheit (celsius) {
-	var fahrenheit = (celsius) * (9/5) + 32;
-	return fahrenheit;
-}
-
-//**Function: which radio button is checked?
-// function convertTemp() {
-// 	var userTemp = parseInput(document.getElementById("userTemp").value);
-// 	if (isNaN(userTemp)) {
-// 		return outputDiv.innerHTML = "Please write a number.";
-// 	}
-// 	if (fahToCelRadio.checked) {
-// 		return outputDiv.innerHTML = toCelsius(userTemp);
-// 	} else if (celToFahRadio.checked) {
-// 		return outputDiv.innerHTML = toFahrenheit(userTemp);
-// 	} else {
-// 		return outputDiv.innerHTML = "Please select whether to convert to celsius or fahrenheit.";
-// 	}
-// }
-
-function convertTemp() {
-	var userTemp = parseInput(document.getElementById("userTemp").value);
-	if (isNaN(userTemp)) {
-		return outputDiv.innerHTML = "Please write a number.";
-	}
-	if (fahToCelRadio.checked) {
-		toCelsius(userTemp);
-		if (userTemp > 32) {
-			return outputDiv.innerHTML = "<span class='red'>" + userTemp + "</span>";
-		} else if (userTemp < 0) {
-			return outputDiv.innerHTML = "<span class='blue'>" + userTemp + "</span>";
-		} else {
-			return outputDiv.innerHTML = "<span class='green'>" + userTemp + "</span>";
-		}
-	} else if (celToFahRadio.checked) {
-		toFahrenheit(userTemp);
-		if (userTemp > 90) {
-			return outputDiv.innerHTML = "<span class='red'>" + userTemp + "</span>";
-		} else if (userTemp < 32) {
-			return outputDiv.innerHTML = "<span class='blue'>" + userTemp + "</span>";
-		} else {
-			return outputDiv.innerHTML = "<span class='green'>" + userTemp + "</span>";
-		}
-	} else {
-		return outputDiv.innerHTML = "Please select whether to convert to celsius or fahrenheit.";
-	}
-}
-
-
-
-// console.log("13: ", validateInput(13));
-// console.log("1.5: ", validateInput(1.5));
-// console.log("13 string: ", validateInput("13"));
-// console.log("1.5 string: ", validateInput("1.5"));
-// console.log("clown: ", validateInput("clown"));
-
-//EVENT LISTENERS
-
-clearButton.addEventListener("click", function() {
-	document.getElementById("tempconverter").reset();
-})
-convertButton.addEventListener("click", convertTemp);
 
 
 
