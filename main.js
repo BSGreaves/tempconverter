8ceb4d6ba71ab49390016bcee5938f1e9e69fd4c
Ben Greaves <bsgreaves@gmail.com>
2017-01-30 20:41:18 -0600

    Started working on JS

--- main.js
+++ main.js
@@ -1,19 +1,68 @@
-function toCelsius () {
+//FUNCTION DECLARATIONS
 
+
+
+//**Function: which radio button is checked?
+function determineConverter(clickEvent) {
+	if (document.getElementById("fahToCelRadio").checked) {
+		//Call a function to turn user input to celsius
+	} else if (document.getElementById("celToFahRadio").checked) {
+		//Call a function to turn user input to fahrenheit
+	} else {
+		//Tell the user they need to select a radio button
+	}
 }
 
-function toFahrenheit () {
+//** Assign a function to be executed when the button is clicked
+button.addEventListener("click", determineConverter);
 
+//** Assign a function to be executed when the user presses enter 
+document.addEventListener("keypress", function (e) {
+	if (13 === e.which) {
+		determineConverter();
+	}
 }
 
-// Get a reference to the button element in the DOM
+
+
+//**Function: F to C
+function toCelsius(userFahrenheit) {
+	var outputCelsius = (userFahrenheit - 32) * (5/9);
+	return outputCelsius;
+}
+
+
+//**Function: C to F
+function toFahrenheit (userCelsius) {
+	var outputFahrenheit = (userCelsius) * (9/5) + 32;
+	return outputFahrenheit;
+}
+
+
+
+//**Function to turn user input into an integer
+function validateInput(userInput) {
+	userInput = userInput.parseInt;
+	if (Number.isInteger(userInput)) {
+		//Do something
+	} else {
+		//throw an error at the user
+	}
+}
+
+//**Get a reference to the button element in the DOM
 var button = document.getElementById("converter");
 
-// This function should determine which conversion should
-// happen based on which radio button is selected.
-function determineConverter (clickEvent) {
-  console.log("event", clickEvent);
-}
 
-// Assign a function to be executed when the button is clicked
-button.addEventListener("click", determineConverter);
+
+
+//VARIABLES
+
+//**Grab the variable by DOM
+var outputDiv = document.getElementById("outputDiv")
+var tempToConvert = document.getElementById("userTemp").innerhtml
+
+
+
+
+
