e2b82b14a78191c8477158054e6b04cee8d24824
Ben Greaves <bsgreaves@gmail.com>
2017-02-01 18:26:38 -0600

    Removed some comments

--- main.js
+++ main.js
@@ -10,25 +10,21 @@ var mainForm = document.getElementById("mainForm");
 
 //FUNCTION DECLARATIONS
 
-//Turns input into a float
 //Decided to use parsefloat so that any decimels will be used in the conversion math
 function inputToNumber(input) {
 	return input = parseFloat(input);
 }
 
-//Fahrenheit to Celsius, and rounds it off
 function toCelsius(fahrenheit) {
 	var celsius = (fahrenheit - 32) * (5/9);
 	return celsius = Math.round(celsius);
 }
 
-//Celsius to Fahrenheit, and rounds it off
 function toFahrenheit (celsius) {
 	var fahrenheit = (celsius) * (9/5) + 32;
 	return fahrenheit = Math.round(fahrenheit);
 }
 
-//Determines the color of the text, if converting to Fahrenheit
 function fahDetermineColor (temp) {
 	if (temp > 90) {
 		return "red";
@@ -39,7 +35,6 @@ function fahDetermineColor (temp) {
 	}
 }
 
-//Determines the color of the text, if converting to Celsius
 function celDetermineColor (temp) {
 	if (temp > 32) {
 		return "red";
@@ -50,12 +45,10 @@ function celDetermineColor (temp) {
 	}
 }
 
-//Outputs the result, in the correct color
 function outputToDiv (div, color, temp, scale) {
 	return div.innerHTML = "<span class='" + color + "'>" + temp + " degrees " + scale + "</span>";
 }
 
-//The main function which is tiggered by the user
 function convertTemp() {
 	var userTemp = inputToNumber(document.getElementById("userInput").value);
 	if (isNaN(userTemp)) {
@@ -76,20 +69,19 @@ function convertTemp() {
 
 //EVENT LISTENERS
 
-//Clear button
 clearButton.addEventListener("click", function() {
 	document.getElementById("mainForm").reset();
 	outputDiv.innerHTML = "";
 })
-//Convert button
 convertButton.addEventListener("click", convertTemp);
 
-//In case they hit enter 
+//In case they hit enter
 mainForm.addEventListener("keypress", function (e) {
 	if (13 === e.keyCode) {
 		convertTemp();
 	}
 });
+
 
 
 
